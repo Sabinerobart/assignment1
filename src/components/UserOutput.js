d0264da6a9bf03b664f6e0c51acd05b5adbe6386
Sabine Robart <sabine.robart@protonmail.com>
2019-09-29 11:09:44 +0200

    [Add] React.Fragment instead of a div

--- src/components/UserOutput.js
+++ src/components/UserOutput.js
@@ -2,10 +2,10 @@ import React from "react";
 
 const UserOutput = props => {
   return (
-    <div>
+    <React.Fragment>
       <p>{props.userName}</p>
       <p>{props.text}</p>
-    </div>
+    </React.Fragment>
   );
 };
 
